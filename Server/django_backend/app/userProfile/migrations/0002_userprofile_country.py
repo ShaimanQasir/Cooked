8f6a925d28d30cb341fea8f7e843686fef1f2161
ShaimanQasir <shamshaiman312@gmail.com>
2026-07-22 14:23:27 +0500

    chore(backend): add database migration for userprofile country field

---
+++ Server/django_backend/app/userProfile/migrations/0002_userprofile_country.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('userProfile', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='userprofile',
+            name='country',
+            field=models.CharField(blank=True, max_length=100),
+        ),
+    ]
